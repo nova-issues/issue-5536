1f08faa75e0046ae8a798c3e553e3f2f022b3b6b
Mior Muhammad Zaki <crynobone@gmail.com>
2023-05-15 20:37:33 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -36,7 +36,7 @@ class UserTableSeeder extends Seeder
 
         User::forceCreate([
             'name' => 'Laravel Nova',
-            'email' => 'hello@iddigital.com.au',
+            'email' => 'hello@laravel.com',
             'password' => $password,
         ]);
     }
